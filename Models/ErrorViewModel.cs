18f5c7fe96665fe6c0e8e98913f4baadb18c2aa0
Your Name <you@example.com>
2022-12-04 17:30:20 -0300

    Creando proyecto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp5_2022_agussarroyoo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
